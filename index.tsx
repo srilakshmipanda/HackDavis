891feadeb04ba1d7d1b8f438f4de1e5a91b4ca43
ssaneg <123612577+ssaneg@users.noreply.github.com>
2023-05-20 18:33:35 -0700

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import { useLoadScript } from "@react-google-maps/api";
+import Map from "../components/map";
+
+export default function Home() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyCNG0IAvVs4e34OJpUp-z87p5Q6dOVEf4c",
+    libraries: ["places"],
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
+}
---
+++ map.tsx
@@ -0,0 +1,156 @@
+import { useState, useMemo, useCallback, useRef, useEffect } from "react";
+import * as LatLngLiteral from "leaflet";
+import * as L from "leaflet";
+import axios from 'axios';
+import Dropdown from "./Dropdown";
+
+
+import {
+  GoogleMap,
+  Marker,
+  DirectionsRenderer,
+  Circle,
+  MarkerClusterer,
+} from "@react-google-maps/api";
+import Places from "./places";
+import Distance from "./distance";
+
+type LatLngLiteral = google.maps.LatLngLiteral;
+type DirectionsResult = google.maps.DirectionsResult;
+type MapOptions = google.maps.MapOptions;
+
+export default function Map() {
+  const [office, setOffice] = useState<LatLngLiteral>();
+  const [directions, setDirections] = useState<DirectionsResult>();
+  const mapRef = useRef<GoogleMap>();
+  /*const center = useMemo<LatLngLiteral>(
+    () => ({ lat: 43.45, lng: -80.49 }),
+    []
+  );*/
+  const [center, setCenter] = useState({ lat: 0, lng: 0 });
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      setCenter({ lat: latitude, lng: longitude });
+    });
+  }, []);
+  
+
+  const options = useMemo<MapOptions>(
+    () => ({
+      mapId: "50b6b3b1d1c6cb1b",
+      disableDefaultUI: true,
+      clickableIcons: false,
+    }),
+    []
+  );
+  const onLoad = useCallback((map) => (mapRef.current = map), []);
+  const houses = useMemo(() => generateHouses(center), [center]);
+
+  const fetchDirections = (house: LatLngLiteral) => {
+    if (!office) return;
+
+    const service = new google.maps.DirectionsService();
+    service.route(
+      {
+        origin: house,
+        destination: office,
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (result, status) => {
+        if (status === "OK" && result) {
+          setDirections(result);
+        }
+      }
+    );
+  };
+
+  const dropdownoptions = [
+    { value: "Theft", label: "Theft" },
+    { value: "Assault", label: "Assault" },
+  ];
+
+  return (
+    <div className="container">
+      <div className="controls">
+        <h1>Am I Safe?</h1>
+        <Places
+          setOffice={(position) => {
+            setOffice(position);
+            mapRef.current?.panTo(position);
+          }}
+        />
+        {!office && <p>Enter the location of the suspicious activity.</p>}
+        {directions && <Distance leg={directions.routes[0].legs[0]} />}
+        <Dropdown placeHolder="Select..." dropdownoptions={dropdownoptions}/>
+      </div>
+      
+      <div className="map">
+        <GoogleMap
+          zoom={10}
+          center={center}
+          mapContainerClassName="map-container"
+          options={options}
+          onLoad={onLoad}
+        >
+          {directions && (
+            <DirectionsRenderer
+              directions={directions}
+              options={{
+                polylineOptions: {
+                  zIndex: 50,
+                  strokeColor: "#1976D2",
+                  strokeWeight: 5,
+                },
+              }}
+            />
+          )}
+
+          {office && (
+            <>
+              <Marker
+                position={office}
+              />
+
+              <MarkerClusterer>
+                {(clusterer) =>
+                  houses.map((house) => (
+                    <Marker
+                      key={house.lat}
+                      position={house}
+                      clusterer={clusterer}
+                      onClick={() => {
+                        fetchDirections(house);
+                      }}
+                    />
+                  ))
+                }
+              </MarkerClusterer>
+            </>
+          )}
+        </GoogleMap>
+      </div>
+    </div>
+  );
+}
+
+const defaultOptions = {
+  strokeOpacity: 0.5,
+  strokeWeight: 2,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+};
+
+const generateHouses = (position: LatLngLiteral) => {
+  const _houses: Array<LatLngLiteral> = [];
+  for (let i = 0; i < 100; i++) {
+    const direction = Math.random() < 0.5 ? -2 : 2;
+    _houses.push({
+      lat: position.lat + Math.random() / direction,
+      lng: position.lng + Math.random() / direction,
+    });
+  }
+  return _houses;
+};
